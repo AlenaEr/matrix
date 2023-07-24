451802de6ed005f7d35b7f5a9b1e145e29b4f0a8
Alena <alana.eremina@gmail.com>
2023-07-24 13:56:37 +0200

    AddTwoNumbers leetcode

---
+++ src/main/java/leetcode/AddTwoNumbersLinkedList.java
@@ -0,0 +1,65 @@
+package leetcode;
+
+/**
+ * https://leetcode.com/problems/add-two-numbers/description/
+ * You are given two non-empty linked lists representing two non-negative integers.
+ * The digits are stored in reverse order, and each of their nodes contains a single digit.
+ * Add the two numbers and return the sum as a linked list.
+ * You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class AddTwoNumbersLinkedList {
+    public static void main(String[] args) {
+        ListNode l1 = new ListNode(2, new ListNode(4, new ListNode(3)));
+        ListNode l2 = new ListNode(5, new ListNode(6, new ListNode(4)));
+
+        ListNode result = addTwoNumbers(l1, l2);
+        printLinkedList(result);
+    }
+
+    private static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = new ListNode(0);
+        ListNode curr = head;
+        int carry = 0;
+        while (l1 != null || l2 != null) {
+            int sum = carry;
+            if (l1 != null) {
+                sum = sum + l1.val;
+                l1 = l1.next;
+            }
+
+            if (l2 != null) {
+                sum = sum + l2.val;
+                l2 = l2.next;
+            }
+            carry = sum / 10;
+            sum = sum % 10;
+            curr.next = new ListNode(sum);
+            curr = curr.next;
+        }
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+        return head.next;
+    }
+
+    private static void printLinkedList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+    }
+
+
+}
