5bf7fa6bc1c1ce2a37598cf057098350ff55bf17
Alena <alana.eremina@gmail.com>
2023-08-03 10:42:10 +0200

    MergeTwoSortedLists_21

---
+++ src/main/java/leetcode/MergeTwoSortedLists_21.java
@@ -0,0 +1,69 @@
+package leetcode;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * https://leetcode.com/problems/merge-two-sorted-lists/
+ * You are given the heads of two sorted linked lists list1 and list2.
+ * <p>
+ * Merge the two lists into one sorted list. The list should be made by splicing together the nodes of the first two lists.
+ * <p>
+ * Return the head of the merged linked list.
+ * Input: list1 = [1,2,4], list2 = [1,3,4]
+ * Output: [1,1,2,3,4,4]
+ */
+public class MergeTwoSortedLists_21 {
+    public static void main(String[] args) {
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        ListNode mergedList = mergeTwoLists(list1, list2);
+
+        ListNode current = mergedList;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+    }
+
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     * int val;
+     * ListNode next;
+     * ListNode() {}
+     * ListNode(int val) { this.val = val; }
+     * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+     * }
+     */
+
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode temp = new ListNode(-1);
+        ListNode current = temp;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val < list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
+        }
+
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+        return temp.next;
+    }
+}
+
