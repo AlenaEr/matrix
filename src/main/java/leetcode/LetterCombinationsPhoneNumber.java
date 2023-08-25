1a79253880beaba62100ebe2f065e83a001827e8
Alena <alana.eremina@gmail.com>
2023-08-25 18:05:51 +0300

    LetterCombinationsPhoneNumber

---
+++ src/main/java/leetcode/LetterCombinationsPhoneNumber.java
@@ -0,0 +1,72 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
+ * <p>
+ * A mapping of digits to letters (just like on the telephone buttons) is given below. Note that 1 does not map to any letters.
+ * Example 1:
+ * <p>
+ * Input: digits = "23"
+ * Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]
+ * Example 2:
+ * <p>
+ * Input: digits = ""
+ * Output: []
+ * Example 3:
+ * <p>
+ * Input: digits = "2"
+ * Output: ["a","b","c"]
+ * <p>
+ * <p>
+ * Constraints:
+ * <p>
+ * 0 <= digits.length <= 4
+ * digits[i] is a digit in the range ['2', '9'].
+ */
+public class LetterCombinationsPhoneNumber {
+    public static void main(String[] args) {
+        System.out.println(letterCombinations("77"));
+    }
+
+    private static final Map<Character, String> digitToLetters = new HashMap<>();
+
+    static {
+        digitToLetters.put('2', "abc");
+        digitToLetters.put('3', "def");
+        digitToLetters.put('4', "ghi");
+        digitToLetters.put('5', "jkl");
+        digitToLetters.put('6', "mno");
+        digitToLetters.put('7', "pqrs");
+        digitToLetters.put('8', "tuv");
+        digitToLetters.put('9', "wxyz");
+    }
+
+    public static List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+        if (digits == null || digits.isEmpty()) {
+            return result;
+        }
+
+        backtrack("", digits, 0, result);
+        return result;
+    }
+
+    private static void backtrack(String current, String digits, int index, List<String> result) {
+        if (index == digits.length()) {
+            result.add(current);
+            return;
+        }
+
+        char digit = digits.charAt(index);
+        String letters = digitToLetters.get(digit);
+
+        for (char letter : letters.toCharArray()) {
+            backtrack(current + letter, digits, index + 1, result);
+        }
+    }
+}
